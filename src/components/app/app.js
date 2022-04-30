7bc701069b0431d7e4d3dde8859237abd48eb561
web77code <web77@yandex.ru>
2022-04-30 20:40:43 +0300

    Correct project structure

---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import { observer } from 'mobx-react';
+import store from '../../store/store';
+
+function App() {
+  return (
+    <div className="App">
+      text
+    </div>
+  );
+}
+
+export default observer(App);
