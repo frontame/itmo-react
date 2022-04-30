5801fb97fa37fe72a6df53a125f35adfaaa5639b
web77code <web77@yandex.ru>
2022-04-30 19:56:06 +0300

    Added data for first 9 cards in component Publications

---
+++ src/data/publicationsData.js
@@ -0,0 +1,85 @@
+// images
+import publication1 from '../images/publications/publication1.jpg';
+import publication2 from '../images/publications/publication2.jpg';
+import publication3 from '../images/publications/publication3.jpg';
+import publication4 from '../images/publications/publication4.jpg';
+import publication5 from '../images/publications/publication5.jpg';
+import publication6 from '../images/publications/publication6.jpg';
+import publication7 from '../images/publications/publication7.jpg';
+import publication8 from '../images/publications/publication8.jpg';
+import publication9 from '../images/publications/publication9.jpg';
+
+export const publications = [
+  {
+    id: 1,
+    image: publication1,
+    title: 'Topic-driven Ensemble for Online Advertising Generation',
+    authors: 'Egor Nevezhin, Nikolay Butakov, Maria Khodorchenko, Maxim Petrov, Denis Nasonov',
+    limb: 'Online advertising is one of the most widespread ways to reach and increase a target audience for those selling products. Usually having a form of a banner, advertising engages users into visiting a corresponding webpage. Professional generation of banners requires creative and writing skills and a basi…',
+    link: '#'
+  },
+  {
+    id: 2,
+    image: publication2,
+    title: 'Convolutional neural networks with hierarchical context transfe…',
+    authors: 'Ksenia D Mukhina, Alexander A Visheratin, Denis Nasonov',
+    limb: 'In this work, we consider a problem of predicting the next state of a given area using retrospective information. The proposed concept of hierarchical context transfer (HCT) operates on several spatial levels of the input data to overcome major issues of next state prediction problems - density variability, a sig…',
+    link: '#'
+  },
+  {
+    id: 3,
+    image: publication3,
+    title: 'Spatiotemporal Filtering Pipeline for Efficient Social Networks Dat…',
+    authors: 'Ksenia Mukhina, Alexander Visheratin, Denis Nasonov',
+    limb: 'One of the areas that gathers momentum is the investigation of location-based social networks (LBSNs) because the understanding of citizens’ behavior on various scales can help to improve quality of living, enhance urban management, and advance the development of smart cities. But it is widely known that the…',
+    link: '#'
+  },
+  {
+    id: 4,
+    image: publication4,
+    title: 'Peregreen–modular database for efficient storage of historical tim…',
+    authors: 'Alexander Visheratin, Alexey Struckov, Semen Yufa, Alexey Muratov, Denis Nasonov, Nikolay B…',
+    limb: 'The rapid development of scientific and industrial areas, which rely on time series data processing, raises the demand for storage that would be able to process tens and hundreds of terabytes of data efficiently. And by efficiency, one should understand not only the speed of data processing operations execution but als…',
+    link: '#'
+  },
+  {
+    id: 5,
+    image: publication5,
+    title: 'Intelligent sightseeing in immensely manifold cities: Case …',
+    authors: 'Ksenia D Mukhina, Alexander A Visheratin, Denis Nasonov, Lev Manovich',
+    limb: 'In this work, we show how social media data can be used for the improvement of touristic experience. We present an algorithm for automated touristic paths construction. Score function for location depends on three components: location\'s social media popularity and rating, distances of place from others in r…',
+    link: '#'
+  },
+  {
+    id: 6,
+    image: publication6,
+    title: 'User Profiles Matching for Different Social Networks Based …',
+    authors: 'Timur Sokhin, Nikolay Butakov, Denis Nasonov',
+    limb: 'It is common practice nowadays to use multiple social networks for different social roles. Although this, these networks assume differences in content type, communications and style of speech. If we intend to understand human behaviour as a key-feature for recommender systems, banking risk assessm…',
+    link: '#'
+  },
+  {
+    id: 7,
+    image: publication7,
+    title: 'Orienteering Problem with Functional Profits for multi-sourc…',
+    authors: 'Ksenia D Mukhina, Alexander A Visheratin, Denis Nasonov',
+    limb: 'Orienteering problem (OP) is a routing problem, where the aim is to generate a path through set of nodes, which would maximize total score and would not exceed the budget. In this paper, we present an extension of classic OP—Orienteering Problem with Functional Profits (OPFP), where the score of …',
+    link: '#'
+  },
+  {
+    id: 8,
+    image: publication8,
+    title: 'Intellectual Execution Scheme of Iterative Computational Models …',
+    authors: 'Mikhail Melnik, Denis A Nasonov, Alexey Liniov',
+    limb: 'In the modern world, with the growth of the volume of processed data arrays, the logic of solving problems also becomes more complex. This leads more and more often to the need to use high-performance computational clusters, such as supercomputers. Created m…',
+    link: '#'
+  },
+  {
+    id: 9,
+    image: publication9,
+    title: 'Urban events prediction via convolutional neural networks a…',
+    authors: 'Ksenia D Mukhina, Alexander A Visheratin, Denis Nasonov',
+    limb: 'In today’s world, it is crucial to be proactive and be prepared for events which are not happening yet. Thus, there is no surprise that in the field of social media analysis the research agenda has moved from the development of event detection methods to a brand new area - event prediction models...',
+    link: '#'
+  },
+];
