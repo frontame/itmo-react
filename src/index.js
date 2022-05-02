3800962e3d5d57f0e588d57d99b0fdd768fef34b
web77code <web77@yandex.ru>
2022-05-02 11:25:45 +0300

    feat: Added OpenSans fonts to project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import OpenSansFonts from './fonts';
 import App from './components/app/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <OpenSansFonts />
     <App />
   </React.StrictMode>
 );
