73fe7f7b094221a56217ce0aba77c51f90df8ef4
web77code <web77@yandex.ru>
2022-05-03 19:45:43 +0300

    content: add data for publications card

--- src/data/publicationsData.js
+++ src/data/publicationsData.js
@@ -8,6 +8,24 @@ import publication6 from '../images/publications/publication6.jpg';
 import publication7 from '../images/publications/publication7.jpg';
 import publication8 from '../images/publications/publication8.jpg';
 import publication9 from '../images/publications/publication9.jpg';
+import publication10 from '../images/publications/publication10.jpg';
+import publication11 from '../images/publications/publication11.jpg';
+import publication12 from '../images/publications/publication12.jpg';
+import publication13 from '../images/publications/publication13.jpg';
+import publication14 from '../images/publications/publication14.jpg';
+import publication15 from '../images/publications/publication15.jpg';
+import publication16 from '../images/publications/publication16.jpg';
+import publication17 from '../images/publications/publication17.jpg';
+import publication18 from '../images/publications/publication18.jpg';
+import publication19 from '../images/publications/publication19.jpg';
+import publication20 from '../images/publications/publication20.jpg';
+import publication21 from '../images/publications/publication21.jpg';
+import publication22 from '../images/publications/publication22.jpg';
+import publication23 from '../images/publications/publication23.jpg';
+import publication24 from '../images/publications/publication24.jpg';
+import publication25 from '../images/publications/publication25.jpg';
+import publication26 from '../images/publications/publication26.jpg';
+import publication27 from '../images/publications/publication27.jpg';
 
 const publicationsData = [
   {
@@ -85,6 +103,165 @@ const publicationsData = [
     title: 'Urban events prediction via convolutional neural networks a…',
     authors: 'Ksenia D Mukhina, Alexander A Visheratin, Denis Nasonov',
     limb: 'In today’s world, it is crucial to be proactive and be prepared for events which are not happening yet. Thus, there is no surprise that in the field of social media analysis the research agenda has moved from the development of event detection methods to a brand new area - event prediction models...',
+    link: '#',
+  },
+  {
+    id: 10,
+    image: publication10,
+    title: 'Ensemble-based method of answers retrieval for domain spe…',
+    authors:
+      'Iskander Safiulin, Nikolay Butakov, Dmitriy Alexandrov, Denis Nasonov',
+    limb: 'Many companies want or prefer to use chatbot systems to provide smart assistants for accompanying human specialists especially newbies with automatic consulting. Implementation of a really useful smart assistant for a specific domain requires a knowledge base for this domain, that often e…',
+    link: '#',
+  },
+  {
+    id: 11,
+    image: publication11,
+    title: 'Evaluation of modern tools and techniques for storing time-serie…',
+    authors:
+      'Alexey Struckov, Semen Yufa, Alexander A Visheratin, Denis Nasonov',
+    limb: 'Time series data as its analysis and applications recently have become increasingly important in different areas and domains. Many fields of science and industry rely on storing and processing large amounts of time series - economics and finance, medicine, the Internet of Things, environmental protection, …',
+    link: '#',
+  },
+  {
+    id: 12,
+    image: publication12,
+    title: 'Workflow scheduling using neural networks and reinforcement lear…',
+    authors: 'Mikhail Melnik, Denis Nasonov',
+    limb: 'The development of information technologies entails a nonlinear growth of both volumes of data and the complexity of data processing itself. Scheduling is one of the main components for optimizing the operation of   the computing system. Currently, there are a large number of scheduling algorithms. However, even in spite of existing hybrid sch…',
+    link: '#',
+  },
+  {
+    id: 13,
+    image: publication13,
+    title: 'Building City-Scale Walking Itineraries Using Large Geospatia…',
+    authors: 'Ksenia D Mukhina, Alexander A Visheratin, Denis Nasonov',
+    limb: "Nowadays, social networks play an important role in many aspects of people's life and in traveling in particular. People share their experience and opinions not only on specialized sites, like TripAdvisor, but also in social networks, e.g. Instagram. Combining information from different sources we can ge…",
+    link: '#',
+  },
+  {
+    id: 14,
+    image: publication14,
+    title: 'Forecasting of the Urban Area State Using Convolutional Neural…',
+    authors:
+      'Ksenia D Mukhina, Alexander A Visheratin, Gali-Ketema Mbogo, Denis Nasonov',
+    limb: 'Active development of modern cities requires not only efficient monitoring systems but furthermore forecasting systems that can predict future state of the urban area with high accuracy. In this work we present a method for urban area prediction based on geospatial activity of users in social network. One of the …',
+    link: '#',
+  },
+  {
+    id: 15,
+    image: publication15,
+    title: 'Multiscale event detection using convolutional quadtrees and ada…',
+    authors:
+      'A A Visheratin, K D Mukhina, A K Visheratina, D Nasonov, A V Boukhanovsky',
+    limb: 'Increasing popularity of social networks made them a viable data source for many data mining applications and event detection is no exception. Researchers aim not only to find events that happen in networks but more importantly to identify and locate events occurring in the real world. In this paper, we …',
+    link: '#',
+  },
+  {
+    id: 16,
+    image: publication16,
+    title: '…',
+    authors:
+      'Nikolay Butakov, Maxim Petrov, Ksenia Mukhina, Denis Nasonov, Sergey Kovalchuk',
+    limb: 'Data provided by social media becomes an increasingly important analysis material for social scientists, market analysts, and other stakeholders. Diversity of interests leads to the emergence of a variety of crawling techniques and programming solutions. Nevertheless, these solutions have a lack of flexibility to sati…',
+    link: '#',
+  },
+  {
+    id: 17,
+    image: publication17,
+    title:
+      'The multi-level adaptive approach for efficient execution of multi-sc…',
+    authors:
+      'D Nasonov, N Butakov, M Melnik, A Visheratin, A Linev, P Shvets, S Sobolev, K Mukhina',
+    limb: 'Today advanced research is based on complex simulations which require a lot of computational resources that usually are organized in a very complicated way from technical part of the view. It means that a scientist from physics, biology or even sociology should struggle with all technical is…',
+    link: '#',
+  },
+  {
+    id: 18,
+    image: publication18,
+    title:
+      'Precedent-Based Approach for the Identification of Deviant Behavio…',
+    authors:
+      'Anna V Kalyuzhnaya, Nikolay O Nikitin, Nikolay Butakov, Denis Nasonov',
+    limb: 'The current paper is devoted to a problem of deviant users’ identification in social media. For this purpose, each user of social media source should be described through a profile that aggregates open information about him/her within the special structure. Aggregated user profiles are formally describ…',
+    link: '#',
+  },
+  {
+    id: 19,
+    image: publication19,
+    title: 'Blockchain-based transaction integrity in distributed big data …',
+    authors: 'Denis Nasonov, Alexander A Visheratin, Alexander Boukhanovsky',
+    limb: 'Today Big Data occupies a crucial part of scientific research areas as well as in the business analysis of large companies. Each company tries to find the best way to make generated Big Data sets valuable and profitable. However, in most cases, companies have not enough opportunities and budget t…',
+    link: '#',
+  },
+  {
+    id: 20,
+    image: publication20,
+    title: 'Towards a scenario-based solution for extreme metocean event sim…',
+    authors: 'A Kalyuzhnaya, D Nasonov, S Ivanov, S Kosukhin, A Boukhanovsky',
+    limb: 'Today, metocean investigations, combined with forecasts and analysis of extreme events, require new design and development approaches because of their complexity. Extreme metocean events forecasting and prevention is an urgent computing task from decision-making and for reaction point of view.',
+    link: '#',
+  },
+  {
+    id: 21,
+    image: publication21,
+    title:
+      'Storage tier-aware replicative data reorganization with prioritization…',
+    authors:
+      'Anton Spivak, Andrew Razumovskiy, Denis Nasonov, Alexander Boukhanovsky, Anton Redice',
+    limb: 'The importance of data collection, processing, and analysis is rapidly growing. Big Data technologies are in high demand in many fields, including bio-informatics, hydrometeorology, and high energy physics. One of the most popular computational paradigms used in large data processing fra…',
+    link: '#',
+  },
+  {
+    id: 22,
+    image: publication22,
+    title: 'Hybrid scheduling algorithm in early warning systems',
+    authors: 'A Visheratin, M Melnik, D Nasonov, N Butakov, A Boukhanovsky',
+    limb: 'The development of an efficient Early Warning System (EWS) is essential for the prediction and prevention of imminent natural hazards. In addition to providing a computationally intensive infrastructure with extensive data transfer, high-execution reliability and hard-deadline satisfaction are important req…',
+    link: '#',
+  },
+  {
+    id: 23,
+    image: publication23,
+    title:
+      'Distributed data-driven platform for urgent decision making in car…',
+    authors:
+      'Sergey V Kovalchuk, Evgeniy Krotov, Pavel A Smirnov, Denis A Nasonov, Alexey N Yakovlev',
+    limb: 'his paper presents ongoing research aimed at developing a data-driven platform for clinical decision support systems (DSSs) that require integration and processing of various data sources within a single solution. Resource management is developed within a framework of an urgent computing approach to address …',
+    link: '#',
+  },
+  {
+    id: 24,
+    image: publication24,
+    title: 'Byzantine fault-tolerant and semantic-driven consensus proto…',
+    authors: 'Stepan Rakitin, Alexander A Visheratin, Denis Nasonov',
+    limb: 'To provide fault tolerance, modern distributed storage systems use specialized network topologies and consensus protocols that create high overheads. The main disadvantage of existing specialized topologies is a difficulty to implement an efficient data placement that takes into account locality of the data. In scie…',
+    link: '#',
+  },
+  {
+    id: 25,
+    image: publication25,
+    title: 'Adaptive performance model for dynamic scaling Apache Spark St…',
+    authors: 'Max Petrov, Nikolay Butakov, Denis Nasonov, Mikhail Melnik',
+    limb: 'Nowadays, data volumes increase exceptionally, a lot of information comes from different sources, for example, from mobile phones, sensors, traffic, etc. All information from these sources can be represented as a data streams, which can grow up and fall in time in their size. In the first case, data proce…',
+    link: '#',
+  },
+  {
+    id: 26,
+    image: publication26,
+    title: 'Hybrid evolutionary workflow scheduling algorithm for dynami…',
+    authors:
+      'D Nasonov, A Visheratin, N Butakov, N Shindyapina, M Melnik, A Boukhanovsky',
+    limb: 'he optimal workflow scheduling is one of the most important issues in heterogeneous distributed computational environments. Existing heuristic and evolutionary scheduling algorithms have their advantages and disadvantages. In this work we propose a hybrid algorithm based on heuristic methods…',
+    link: '#',
+  },
+  {
+    id: 27,
+    image: publication27,
+    title: 'Execution time estimation for workflow scheduling',
+    authors:
+      'A Chirkin, Adam SZ Belloum, S Kovalchuk, Marc X Makkes, M Melnik, A Visheratin, D Nasonov',
+    limb: 'Estimation of the execution time is an important part of the workflow scheduling problem. The aim of this paper is to highlight common problems in estimating the workflow execution time and propose a solution that takes into account the complexity and the stochastic aspects of the workflow compone…',
     link: '#',
   },
 ];
