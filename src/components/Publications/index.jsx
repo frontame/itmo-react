ccd639ee9eefd53b3c78785932dc88cef79727de
web77code <web77@yandex.ru>
2022-05-03 19:33:14 +0300

    feat: added uuid package to project

--- src/components/Publications/index.jsx
+++ src/components/Publications/index.jsx
@@ -1,5 +1,6 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import { v4 as uuidv4 } from 'uuid';
 import { Section, Container, Title } from './styles';
 import store from '../../store/store';
 import Publication from '../Publication';
@@ -12,11 +13,14 @@ function Publications() {
       <Container>
         <Title>Публикации</Title>
         <Swiper spaceBetween={30} slidesPerView={3}>
-          {data.map((publication) => (
-            <SwiperSlide key={publication.id}>
-              <Publication data={publication} />
-            </SwiperSlide>
-          ))}
+          {data.map((publication) => {
+            const uid = uuidv4();
+            return (
+              <SwiperSlide key={uid}>
+                <Publication data={publication} />
+              </SwiperSlide>
+            );
+          })}
         </Swiper>
       </Container>
     </Section>
