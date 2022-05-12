3a526897a37dc892d8aecdb10babe8051c96bfb8
web77code <web77@yandex.ru>
2022-05-12 18:56:29 +0300

    fix: resolve another one conflict during merge from develop

--- src/store/publications.js
+++ src/store/publications.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 import { action, makeObservable, observable, runInAction } from 'mobx';
 import { v4 as uuidv4 } from 'uuid';
 import publicationsData from '../data/publicationsData';
-=======
-import { action, makeObservable, observable } from 'mobx';
-import industrialData from '../data/industrialData';
->>>>>>> develop
 
 class Publications {
   data = [];
@@ -19,16 +14,11 @@ class Publications {
     runInAction(this.loadPublications);
   }
 
-<<<<<<< HEAD
   loadPublications = () => {
     publicationsData.forEach((item) => {
       const uid = uuidv4();
       this.data.push({ uid, ...item });
     });
-=======
-  getPublications = () => {
-    this.data = industrialData;
->>>>>>> develop
   };
 }
 
