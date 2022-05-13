41fbd4dc999aa7408db4381f8dadda6452943d58
web77code <web77@yandex.ru>
2022-05-13 08:25:28 +0300

    feat: add theme provider and colors to it

---
+++ src/themes/basic-theme.jsx
@@ -0,0 +1,13 @@
+const basicTheme = {
+  primaryColor: '#001337',
+  primaryColorDark: '#000d26',
+  secondaryColor: '#009fac',
+  secondaryColorLight: '#e0f0ff',
+  thirdColor: '#ffffff',
+  bgColorPrimary: '#001337',
+  bgColorPrimaryDark: '#000d26',
+  bgColorSecondary: '#009fac',
+  bgColorSecondaryLight: '#e0f0ff',
+};
+
+export default basicTheme;
