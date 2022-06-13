8bb7de73e0a83059acf24bff9255cb9c6856e00c
web77code <web77@yandex.ru>
2022-06-13 12:59:00 +0300

    feat: added email pattern for email field in form

--- src/components/CallToAction/index.jsx
+++ src/components/CallToAction/index.jsx
@@ -71,6 +71,7 @@ const CallToAction = () => {
               placeholder="E-mail*"
               {...register('email', {
                 required: 'Поле обязательно для заполнения',
+                pattern: /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i,
               })}
               style={
                 errors?.email && { background: '#FFAAAA', color: '#000000' }
